e3d55db6186da89284d0e6449908eb0e6d9ea635
marcuswentz <markzcollege404@gmail.com>
2023-04-29 08:18:50 -0400

    nftDescriptorAddress deployed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,21 +4,32 @@ const {Contract, ContractFactory, utils, BigNumber} = require('ethers')
 const WETH9 = require('../WETH9.json')
 const bn = require('bignumber.js')
 
-deployUniswapV3Factory()
+deploySepoliaUniswapV3()
 
-async function deployUniswapV3Factory() {
+async function deploySepoliaUniswapV3() {
+  
   const [owner, signer2] = await ethers.getSigners();
+  console.log(owner.address)
   const provider = waffle.provider;
   
-  console.log(owner.address)
+  // Factory = new ContractFactory(artifacts.UniswapV3Factory.abi,artifacts.UniswapV3Factory.bytecode,owner)
+  // factory = await Factory.deploy();
+  // console.log('Factory', factory.address)
   
-  // Weth = new ContractFactory(!!!!,!!!!,owner)
-  // weth = await Weth.deploy();
-  // console.log('weth', weth.address)
+  let factoryAddress = "0x4e172dbef259c46de56ef953f29215640a664b31"
+  let wethAddress = "0xc1202e7d42655F23097476f6D48006fE56d38d4f"
   
-  Factory = new ContractFactory(artifacts.UniswapV3Factory.abi,artifacts.UniswapV3Factory.bytecode,owner)
-  factory = await Factory.deploy();
-  console.log('Factory', factory.address)
+  // SwapRouter = new ContractFactory(artifacts.SwapRouter.abi,artifacts.SwapRouter.bytecode,owner)
+  // swapRouter = await SwapRouter.deploy(factoryAddress,wethAddress);
+  // console.log('SwapRouter', swapRouter.address)
+  
+  let swapRouterAddress = "0xEEf4f98dD12FcC6193cCCE792F3983803D0b56eD" 
+  
+  // NFTDescriptor = new ContractFactory(artifacts.NFTDescriptor.abi,artifacts.NFTDescriptor.bytecode,owner)
+  // nftDescriptor = await NFTDescriptor.deploy();
+  // console.log('NFTDescriptor', nftDescriptor.address)
+
+  let nftDescriptorAddress = "0xc26ea02fb53594952b64559278bD0622555584e4"
   
 }
 
